19fb1d8b4f81ba73679cfa5c2819a5b12cde5778
Hassan Ali <hassanramadan4443@gmail.com>
2023-05-20 13:46:09 +0300

    Initial Commit

---
+++ ComposableScrumdinger/ComposableScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableScrumdingerApp.swift
+//  ComposableScrumdinger
+//
+//  Created by mac on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ComposableScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
