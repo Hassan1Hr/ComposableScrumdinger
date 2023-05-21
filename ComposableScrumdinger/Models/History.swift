87af1209e43d454e8092dca90a0c88dbc37186e6
Hassan Ali <hassanramadan4443@gmail.com>
2023-05-21 18:09:07 +0300

    Create History model to hold scrum history

---
+++ ComposableScrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  ComposableScrumdinger
+//
+//  Created by mac on 21/05/2023.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
