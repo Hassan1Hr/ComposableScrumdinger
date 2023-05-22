c87360819fc959b2f056b1138543d6d162b45e69
Hassan Ramadan <hassanramadan4443@gmail.com>
2023-05-22 21:51:11 +0300

    Handling and wrapper customs Errors  (#4)
    * Add a player property that returns AVPlayer.sharedDingPlayer
    
    * Add AVPlayer+Ding to defines the sharedDingPlayer object, which plays the ding.wav resource.
    
    * Add NewScrumSheet to can create and edit scrum
    
    * Create History model to hold scrum history
    
    * Configure AVPlayer+Ding to play sound of speaker on meetingview
    
    * Add ScrumStore to handle save Scrum Speaker data to file system
    
    * Make app load Scrum data from file system other than dump data array
    
    * Add ErrorWrapper to handle read saved data error
    
    * Handle reading  saved data errors

--- ComposableScrumdinger/Models/DailyScrum.swift
+++ ComposableScrumdinger/Models/DailyScrum.swift
@@ -6,7 +6,7 @@
 //
 import Foundation
 
-struct DailyScrum: Identifiable {
+struct DailyScrum: Identifiable, Codable {
     let id: UUID
     var title: String
     var attendees: [Attendee]
@@ -20,6 +20,8 @@ struct DailyScrum: Identifiable {
         }
     }
     var theme: Theme
+    var history: [History] = []
+    
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
         self.id = id
         self.title = title
@@ -34,7 +36,7 @@ struct DailyScrum: Identifiable {
 }
 
 extension DailyScrum {
-    struct Attendee: Identifiable {
+    struct Attendee: Identifiable, Codable {
         let id: UUID
         var name: String
         
---
+++ ComposableScrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWraper.swift
+//  ComposableScrumdinger
+//
+//  Created by mac on 22/05/2023.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
